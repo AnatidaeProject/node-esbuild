676d394cd0625e209e3ccce8352d31f144fa069d
Brian McBride <Brian-McBride@users.noreply.github.com>
2021-03-13 14:15:45 -0800

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  projects: [
+    '<rootDir>/packages/node-esbuild',
+    '<rootDir>/e2e/node-esbuild-e2e',
+  ],
+};
---
+++ packages/node-esbuild/src/generators/node-esbuild/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NodeEsbuildGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
